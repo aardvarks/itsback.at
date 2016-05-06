5f02aa5158511bb78899aa9528a8049950d5b780
Owen Garland <owen.garland@clock.co.uk>
2016-05-06 19:11:12 +0100

    Added queries to database, but still needs to be made into a class via magic

--- database.js
+++ database.js
@@ -1,13 +1,23 @@
-var MongoClient = require('mongodb').MongoClient
+'use strict'
 
-class Database {
-  constructor (url) {
-    MongoClient.connect(url, function(err, db) {
-      console.log("Connected succesfully to server")
-    })
-  }
+var MongoClient = require('mongodb')
+  , url = 'mongodb://localhost:27017/itsback'
+	, db
 
-  addReport () {
+MongoClient.connect(url, (err, database) => {
+  if (err) return console.log(err)
+  db = database
 
-  }
+	addReport('google.com', (err, data) => { console.log(data.value.domain) })
+	findReport('google.com', (err, data) => { console.log(data) })
+})
+
+function addReport (domain, cb) {
+	db.collection('domains').findAndModify({ domain: domain }, {}, { $inc: { reported: 1 } }, { new: true , upsert: true }, cb)
 }
+
+function findReport (domain, cb) {
+	db.collection('domains').findOne({ domain: domain }, { _id: false, reported: true }, cb)
+}
+
+
