5f0875224895a8834801b972f58ae53eac69233e
Matthew Elphick <matthew.elphick@clock.co.uk>
2016-05-17 19:57:25 +0100

    Fix Owen's stuff

--- test/find-domain-and-port.test.js
+++ test/find-domain-and-port.test.js
@@ -1,18 +1,18 @@
 const assert = require('assert')
     , findDomainAndPort = require('../lib/find-domain-and-port')
     , fixtures =
-      [ { url: { url: 'http://google.com' } , result: { domain: 'google.com', port: '80' } }
+      [ { url: { url: 'http://google.com' }, result: { domain: 'google.com', port: '80' } }
       , { url: { url: 'https://google.com' }, result: { domain: 'google.com', port: '443' } }
       , { url: { url: 'https://google.com:3000' }, result: { domain: 'google.com', port: '3000' } }
       , { url: { url: 'http://google.com:3000' }, result: { domain: 'google.com', port: '3000' } }
-      , { url: { url: 'http://google.com:3000/path' } , result: { domain: 'google.com', port: '3000' } }
+      , { url: { url: 'http://google.com:3000/path' }, result: { domain: 'google.com', port: '3000' } }
       , { url: { url: 'http://google.com:3000' }, result: { domain: 'google.com', port: '3000' } }
-      , { url: { url: 'google.com' } , result: { domain: 'google.com', port: '80' } }
-      , { url: { url: 'google.com/path' } , result: { domain: 'google.com', port: '80' } }
-      , { url: { url: 'google.com:3000/path' } , result: { domain: 'google.com', port: '3000' } }
+      , { url: { url: 'google.com' }, result: { domain: 'google.com', port: '80' } }
+      , { url: { url: 'google.com/path' }, result: { domain: 'google.com', port: '80' } }
+      , { url: { url: 'google.com:3000/path' }, result: { domain: 'google.com', port: '3000' } }
       , { url: { url: 'google.com:3000' }, result: { domain: 'google.com', port: '3000' } }
+      , { url: { url: '://google.com:3000' }, result: { domain: 'google.com', port: '3000' } }
       ]
-
 
 describe('Test URL parsing logic', () => {
   fixtures.forEach((fixture) => {
